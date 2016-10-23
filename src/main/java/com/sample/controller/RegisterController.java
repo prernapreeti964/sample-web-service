613cefcffb7cf4b2549775075e3966be1d22eee3
ganesh4991 <ganesh4991@gmail.com>
2016-10-23 13:31:18 -0400

    Add html pages

--- src/main/java/com/sample/controller/RegisterController.java
+++ src/main/java/com/sample/controller/RegisterController.java
@@ -2,8 +2,11 @@ package com.sample.controller;
 
 import com.sample.models.facade.UserAccountFacade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -17,10 +20,16 @@ public class RegisterController {
     @Autowired
     UserAccountFacade userAccountFacade;
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
     @ResponseBody
-    public String test() {
+    public String addUser() {
         userAccountFacade.addTestUser();
         return "hello";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String showRegister() {
+        return "redirect:register.html";
     }
 }
